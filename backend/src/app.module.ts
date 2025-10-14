68a38ec2602866cedff2a4d06ba169fa743d7e6c
Pudusa <brsoto@uclv.cu>
2025-10-14 15:46:46 -0400

    feat(backend): create and provide prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
