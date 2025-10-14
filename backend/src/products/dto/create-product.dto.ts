ec99524995dbe70f6498af8a73611fec6e9ca1c5
Pudusa <brsoto@uclv.cu>
2025-10-14 15:54:41 -0400

    feat(products): implement full crud for products resource

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString, IsUUID } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsString() // Expecting a JSON string for the array of URLs
+  @IsNotEmpty()
+  imageUrls: string;
+
+  @IsUUID()
+  managerId: string;
+}
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this.prisma.product.create({ data: createProductDto });
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.prisma.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.prisma.product.findUnique({ where: { id } });
+    if (!product) {
+      throw new NotFoundException(`Product with ID "${id}" not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    await this.findOne(id); // Ensure product exists
+    return this.prisma.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    await this.findOne(id); // Ensure product exists
+    await this.prisma.product.delete({ where: { id } });
+    return { message: `Product with ID "${id}" successfully deleted` };
   }
 }
