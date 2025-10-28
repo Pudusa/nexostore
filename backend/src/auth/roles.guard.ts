d0efecddc1f461613926b83650c1a213ebde2ec6
Pudusa <brsoto@uclv.cu>
2025-10-28 10:12:11 -0400

    feat: add backend and complete initial setup

---
+++ backend/src/auth/roles.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../../generated/prisma/index';
+import { ROLES_KEY } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true; // Si no se especifican roles, se permite el acceso
+    }
+    const { user } = context.switchToHttp().getRequest();
+    return user && user.role && requiredRoles.some((role) => user.role === role);
+  }
+}
